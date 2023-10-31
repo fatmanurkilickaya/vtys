71a471f8018f844ce23d10fe205797ac6aeae68d
Fatma Nur Kılıçkaya <82950952+fatmanurkilickaya@users.noreply.github.com>
2023-10-31 10:10:07 +0300

    Add files via upload

---
+++ DMLSQLDATA.sql
@@ -0,0 +1,22 @@
+USE OKUL_KUTUPHANESI;
+SELECT * FROM ADRESLER;
+
+INSERT INTO ADRESLER VALUES('Sümer','Kocasinan',7,'Kayseri', 38,'Türkiye');
+INSERT INTO ADRESLER VALUES('Sivas','Melikgazi',2,'Kayseri', 38,'Türkiye');
+INSERT INTO ADRESLER VALUES('Sincan','Sincan',78,'Ankara', 06,'Türkiye');
+INSERT INTO ADRESLER VALUES('Karþýyaka','Karþýyaka',123,'Ýzmir', 35,'Türkiye');
+INSERT INTO ADRESLER VALUES('Seyitgazi','Seyitgazi',7,'Eskiþehir', 26,'Türkiye');
+INSERT INTO ADRESLER VALUES('Avanos','Avanos',4,'Nevþehir', 50,'Türkiye');
+INSERT INTO ADRESLER VALUES('Kadýköy','Kadýköy',6,'Ýstanbul', 34,'Türkiye');
+INSERT INTO ADRESLER VALUES('Ortahisar','Ortahisar',8,'Trabzon', 61,'Türkiye');
+
+
+select * from uyeler;
+
+
+INSERT INTO UYELER VALUES('Ayþe','Sezer','K','123456', 'ayse@kayseri.edu.tr', 2);
+INSERT INTO UYELER VALUES('Ahmet','Yazýcý','K','123456', 'ayse@kayseri.edu.tr', 3);
+INSERT INTO UYELER VALUES('Enes','Yücel','K','123456', 'ayse@kayseri.edu.tr', 4);
+INSERT INTO UYELER VALUES('Samet','Kýlýç','K','123456', 'ayse@kayseri.edu.tr', 5);
+INSERT INTO UYELER VALUES('Ýrem','Kaya','K','123456', 'ayse@kayseri.edu.tr', 6);
+INSERT INTO UYELER VALUES('Mustafa','Yýlmaz','K','123456', 'ayse@kayseri.edu.tr', 7);
